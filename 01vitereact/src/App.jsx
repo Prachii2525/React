ae0bff5c90a2c7f02d989a2e6194b2b575c5bdc4
Prachii2525 <prachijanjire@gmail.com>
2024-08-24 18:03:42 +0530

    changes

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Coffee from "./coffee" 
+
+function App() {
+ 
+
+  return (
+    <>
+   <Coffee/>
+   <h2>React with Coffee</h2>
+   </>
+
+  )
+}
+
+export default App
---
+++ customReact/customreact.js
@@ -0,0 +1,11 @@
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        traget: '_blank'
+    },
+    children: 'Click me to visit google'
+
+}
+
+const mainContainer = document.querySelector('#root')
