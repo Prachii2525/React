269c0c023414b016eb3583c9adacd3b3f7b880c3
Prachii2525 <prachijanjire@gmail.com>
2024-10-02 10:33:32 +0530

    updation

--- bg changer/src/App.jsx
+++ bg changer/src/App.jsx
@@ -8,7 +8,9 @@ function App() {
     <>
      <div className="w-full h-screen duration-200"
      style={{backgroundColor: color}}
-     ></div>
+     >
+     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test</div>
+     </div>
     </>
   )
 }
