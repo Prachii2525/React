d64aa8896fabdc859fbb12e0c137ab20f7af9222
Prachiii18 <prachijanjire@gmail.com>
2025-01-22 22:41:49 +0530

    UseEffect

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,44 +1,35 @@
-import { useCallback, useState } from 'react'
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setIsCharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
-
-  const passwordGenerator =useCallback(() => {
-  let pass = ""
-  },
-    [length,numberAllowed,charAllowed,setPassword])
-
-
-    
-  
-      const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
-      const digitChars = '0123456789';
-      const specialChars = '!@#$%^&*()_+';
-
-      const generatePasswordString = (passwordLength: number) => {
-        let characterList = '';
-    
-        const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
-        const digitChars = '0123456789';
-        const specialChars = '!@#$%^&*()_+';
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
- 
-      </h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-
   )
-}
 }
 
 export default App
